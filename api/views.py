0d0e23df4a41a577e1200ca679c3fb269da49ba0
ekrsw <eisuke.koresawa@gmail.com>
2024-08-03 09:21:38 +0900

    excel

--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_api_key.permissions import HasAPIKey
+from rest_framework.permissions import IsAuthenticated
 from .serializers import KPIModelSerializer
 from rest_framework import status
 from kpi.models import KPIModel
 
 class KPIModelView(APIView):
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = KPIModelSerializer
 
     def get(self, request):
