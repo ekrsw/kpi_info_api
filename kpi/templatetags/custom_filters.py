ee007d8dc79a8f057ecfc5e6a96ff3ef656143af
ekrsw <eisuke.koresawa@gmail.com>
2024-08-09 12:44:55 +0900

    buffer color

--- kpi/templatetags/custom_filters.py
+++ kpi/templatetags/custom_filters.py
@@ -1,3 +1,5 @@
+import math
+
 from django import template
 
 register = template.Library()
@@ -10,3 +12,21 @@ def percentage(value):
 def add_values(value1, value2, value3):
     return value1 + value2 + value3
 
+@register.simple_tag
+def buffer(r, n, c):
+    """KPIを達成するためのBufferを計算
+        
+    args:
+        r: KPIの目標値
+        n: 現在の達成件数
+        c: 分母"""
+        
+    # KPIを達成できている場合は単純計算、できていない場合少し複雑。
+    if c == 0:
+        return '-'
+    elif n / c >= r:
+        b = (n / r) - c
+        return int(math.floor(b))
+    else:
+        b = (n - c * r) / (1 - r)
+        return int(math.floor(b))
