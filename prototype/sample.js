ce7ea4c03dc2ca5309aa99fb824634ceac70d519
ekrsw <eisuke.koresawa@gmail.com>
2024-08-16 17:33:50 +0900

    m

---
+++ prototype/sample.js
@@ -0,0 +1,15 @@
+var dataSet = [300, 130, 5, 60, 240]
+
+d3.select("#myGraph")
+  .selectAll("rect")
+  .data(dataSet)
+  .enter()
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", function(d, i){
+    return i * 25;
+  })
+  .attr("width", function(d, i){
+    return d + "px";
+  })
+  .attr("height", "20px")
